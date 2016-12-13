087e2e28307bdd862f5c63ccff2ce112e53891c1
Keith MacKay <kamackay@ncsu.edu>
2016-12-13 18:21:35 -0500

    I don't remember making changes

--- auth.js
+++ auth.js
@@ -4,7 +4,14 @@ var router = express.Router();
 const _ = require('./_');
 
 var f = function (request, response, next) {
-    
+    try {
+        var url = request.url;
+        _.log({ url: url });
+        response.send("OK");
+    } catch (err) {
+        _.err(err);
+        response.status(500).json(err);
+    }
 };
 router.all('*', f);
 
